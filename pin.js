865acae937aaae0e8e71c05fa6239fde01ee6943
shivaansharma <113043195+shivaansharma@users.noreply.github.com>
2023-07-30 16:12:43 +0530

    new

--- pin.js
+++ pin.js
@@ -21,39 +21,7 @@ let pincode = function (e) {
             let newarr = array.map((element) => {
                 return new obj(element.Name, element.District,element.State)
             })
-            let pincode= function(e){
-                e.preventDefault()
-                let value = parseInt(document.querySelector("#input").value)
-                 let url = `https://api.postalpincode.in/pincode/${value}`
-                let prom = fetch(url)
-                prom.then(prom=>prom.json())
-                .then((data)=>{
-                   let data1 = data[0]
-                   let postaloffice = data1.PostOffice
-                   let num =  postaloffice.length
-                   return postaloffice
-                })
-                .then((array)=>{
-                   class obj{
-                    constructor(Name,district,state){
-                          this.Name = Name
-                          this.district=district
-                          this.state = state
-                    }
-                   }
-                  let newarr=  array.map((element)=>{
-                    new obj (array[element].Name,array[element].District,array[element].State)
-                   })
-                   console.log(newarr)
-                   return newarr
-                })
-                .catch((error)=>{
-                    console.log("an error has occured"+error)
-                })
-            }
-            let code = document.querySelector("form")
-             code.addEventListener("submit",pincode)
-            
+            console.log(newarr)
         })
         .catch((error) => {
             console.log("an error has occured" + error)
